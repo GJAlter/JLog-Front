bf5962aa89be9392de55f8918cc76a7436bc7cb4
Jun <gjdeveloper@vers.kr>
2024-04-24 23:54:13 +0900

    테스트 이미지 삭제

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -111,7 +111,6 @@ const LoginPage = () => {
                 </div>
                 <input type="button" onClick={onLoginClick} value={"로그인"} />
             </div>
-            <img src={`${process.env.PUBLIC_URL}/imgs/logout.svg`} alt="logout" />
         </LoginDiv>
     );
 };
